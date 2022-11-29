2ef3d33ef8159b59b68e41e66b77464e03747015
Ardjuhh <ardzomer@hotmail.com>
2022-11-29 12:24:08 +0100

    Update ReadTXT.cs

--- ReadTXT.cs
+++ ReadTXT.cs
@@ -4,12 +4,21 @@ public class ReadFromFile
     {
         // line to read file
         string[] lines = System.IO.File.ReadAllLines(link);
-        // can be made global. But size needs to defined.
-        int numberOfGraphs = 5;
-        int numberOfValues = 81;
-        int[,] sudoku = new int[numberOfGraphs,numberOfValues];
+        int numberOfGraphs = 0;
+        const int numberOfValues = 81;
         int i = 0;
         int j = 0;
+
+        // count number of graphs present in txt doc
+        foreach (string line in lines)
+        {
+            if (line[0] == ' ')
+            {
+                numberOfGraphs++;
+            }
+        }
+
+        int[,] sudoku = new int[numberOfGraphs,numberOfValues];
 
         foreach (string line in lines)
         {
@@ -32,6 +41,81 @@ public class ReadFromFile
                 i = 0;
                 j++;
             }
+        }
+
+        int[] arrayA = new int[9];
+        int[] arrayB = new int[9] {1,2,3,4,5,6,7,8,9};
+
+        // per graph
+        for (int k = 0; k < numberOfGraphs; k++)
+        {        
+            int begin = 0;
+            int end = 21;
+            int position = 0;
+            int counter = 0;
+            const int maxStepSize = 21;
+
+            while(true)
+            {
+                // 3x3 block found and reset to get a new one
+                if (begin + counter == end) 
+                {
+                    // compare two arrays on difference
+                    IEnumerable<int> difference = arrayB.Except(arrayA);
+
+                    int nextElement = 0;
+                    foreach (var g in arrayA)
+                    {
+                        if (g == 0)
+                        {
+                            arrayA[nextElement] = difference.ElementAt(0);
+                        }
+                        nextElement++;
+                        
+                    }
+
+                    // test purposes //////////
+                    System.Console.Write(" ");
+                    foreach (var h in arrayA)
+                    {
+                        Console.Write(h);
+                    }
+                    System.Console.WriteLine(" ");
+                    ////////////////////////////
+                    
+                    if (begin + counter == (numberOfValues/3)) // upper 3 blocks cleared
+                    {
+                        begin = (numberOfValues/3);
+                        end = begin + maxStepSize;
+                    }
+                    else if(begin + counter == ((numberOfValues/3)*2)) // middle 3 blocks cleared
+                    {
+                        begin = ((numberOfValues/3)*2);
+                        end = begin + maxStepSize;
+                    }
+                    else if(begin + counter == numberOfValues) // lower 3 blocks cleared and break 
+                    {
+                        break;
+                    }
+                    else // blocks aren't done yet
+                    {
+                        end = end + 3; 
+                        begin = end - maxStepSize;  
+                    }
+                    counter = 0; 
+                    position = 0;
+                }
+                // if 3 numbers in a block are done then skip to the next line to continue
+                if (counter == 3) {begin = begin+9; counter = 0;}
+                
+                // place flat array in a block
+                arrayA[position] = sudoku[k,begin+counter];
+                System.Console.Write(arrayA[position]);
+                
+                counter++;
+                position++;
+            }
+            System.Console.WriteLine("");
         }
 
         return sudoku;
