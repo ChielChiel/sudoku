2f4a393c47567006b4cc8e0db36e83aff360a0ed
Michiel Schouten <51763911+ChielChiel@users.noreply.github.com>
2022-11-30 15:24:07 +0100

    instructies toegevoegd

--- Solver.cs
+++ Solver.cs
@@ -29,6 +29,8 @@ class Solver {
 
     // Methode moet nog uitgewerkt worden. Maar de basis staat.
     public Bord HillClimb(Bord problem) {
+        // /int[] past_ten_toestand = {evaluatie_waarden_van_vroeger}
+        
         Bord current = problem;
         while (true)
         {
@@ -41,7 +43,15 @@ class Solver {
     }
 
 
-    private Bord Swap(Bord problem) {
+    private Bord Swap(Bord problem, bool random_walk = false) {
+        // bijhouden beste_tot_nu_toe
+        // voordat swap, copy sudoku maken
+        // dan swappen
+        // bord.UpdateEvaluatie() aanroepen
+        // dan chekcen of beter dan beste_tot_nu_toe
+        // na alle swaps, beste_tot_nu_toe returnen
+
+
 
         Random rnd = new Random();
         Bord swapped = problem;
@@ -55,14 +65,17 @@ class Solver {
         int second = rnd.Next(0, blok.Length-1);
         (swapped.sudoku[blok[first]], swapped.sudoku[blok[second]]) = (swapped.sudoku[blok[second]], swapped.sudoku[blok[first]]);
 
-
+        // swapped.UpdateEvaluatie()
         return swapped;
     }
 
-    private int[] GetBlok()
+    private int[] GetBlok(bool returnAll = false)
     {
         //hier moet eigenlijk een array komen van elk blok met de indexen van getallen die geswapt kunnen worden
         int[] blok = new int[] { 0, 1, 2, 9, 10, 11, 18, 19, 20 };
+        
+        
+        // Node node.Verplaatsbaar == true
         return blok;
     }
 
